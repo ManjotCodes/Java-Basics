80b72e87eba837fcca9458272f156c6ccd938bc1
ManjotCodes <110255794+ManjotCodes@users.noreply.github.com>
2022-09-29 21:23:43 +0530

    Add files via upload

---
+++ loops.java
@@ -0,0 +1,14 @@
+public class loops {
+    public static void main(String[] args) {
+        //counter++ = counter= counter+1
+        //for(int counter=0;counter<11;counter++){
+            //System.out.println(counter+);
+
+        //}
+        // We can use i in place of counter
+        for(int i=0;i<11;i++){
+            System.out.println(i);
+        }
+    }
+
+    }
